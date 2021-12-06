16ca614a4a52c52874d71485eb4aaf26a8da8901
Kallycta <83584576+Kallycta@users.noreply.github.com>
2021-12-06 19:53:42 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+$(document).ready(function () {
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
@@ -19,3 +21,96 @@ counters.forEach((item, i) => {
 });
 
 
+// $('.contacts__form').validate({
+//     debug: true
+// });
+function valideForms() {
+  
+    $('form').validate({
+      
+        rules: {
+             name: {
+               required: true,
+               minlength: 2
+             },
+            phone: 'required',
+            email: {
+                required: true,
+                email: true
+            },
+            mess: {
+                required: true,
+                minlength: 10
+            },
+            checkbox: {
+                required: true
+            }
+
+        },
+           messages: {
+               name: {
+                   required: "Пожалуйста, введите свое имя",
+                   minlength: jQuery.validator.format("Введите минимум {0} символа!")
+                 },
+               phone: "Пожалуйста, введите свой номер телефона",
+               email: {
+                 required: "Пожалуйста, введите свою почту",
+                 email: "Неправильно введен адрес почты"
+               },
+               mess: {
+                required: "Пожалуйста, введите ваше сообщение",
+                minlength: jQuery.validator.format("Введите минимум {0} символов!")
+               },
+               checkbox: {
+                required: "Поле обязательно для заполнения"
+               }
+
+        }
+   });
+
+   
+$('form').submit(function(e) {
+    e.preventDefault();
+    if ($(this).valid()) {
+
+   
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+
+        
+
+        $('form').trigger('reset');
+    });
+}
+    return false;
+
+});
+
+
+
+}
+
+valideForms();
+
+
+const triggerSubmit = document.querySelector('.contacts__btn');
+triggerSubmit.addEventListener('submit', () => {
+
+    const modalThanks = document.querySelector('.pageclip-form__success__message');
+    
+    modalThanks.innerHTML = 'Спасибо, в ближайшее время я свяжусь с вами.';
+});
+
+
+
+
+
+});
+
+
+
+// $('input[name=phone]').mask("+7 (999) 999-9999"); 
